6769b14960fcf733f4c76c1b9dc32b6b7e96663d
0xSimeon <simeon.udoh45@gmail.com>
2023-07-27 03:38:56 +0100

    :zap: update scripts both nft contracts

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+
+        vm.startBroadcast();
+
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+
+        vm.stopBroadcast();
+
+        return moodNft;
+    }
+
+    /// @dev programmatically encode an svg file into base64..
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        // Base64 starter
+        string memory baseURI = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
+    }
+}
